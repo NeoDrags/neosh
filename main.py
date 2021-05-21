b26e2c89d8eadaa1f2bbf28aac44ed006e076d51
neodrags <kesavarapu.prateek@gmail.com>
2021-05-21 18:01:45 +0530

    should work on windows

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 from functions import execute_commands, ls, clear
 from pathlib import Path
-import readline
+from pygments.lexers.shell import BashLexer
+from prompt_toolkit.lexers import PygmentsLexer
+from prompt_toolkit.shortcuts import prompt
+from prompt_toolkit.completion import word_completer
+from prompt_toolkit import PromptSession
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 import os
-from termcolor import colored
+from prompt_toolkit.styles import Style
 import getpass
 import socket
 
 def main():
-    readline.parse_and_bind("tab: complete")
+    style = Style.from_dict({
+
+    # Prompt.
+    'username': '#0FFFFF bold',
+    'dir': '#FFFFFF bold',
+    'end': '#0FFFFF bold'
+    })
+
     HOME_DIR = str(Path.home())
     hostname = socket.gethostname()
     history = Path(HOME_DIR + "/.yash_history")
-    if history.is_file():
-        readline.read_history_file(history)
-    else:
-        open(str(history), "a").close()
+    session = PromptSession(history=FileHistory(str(history)))
+
     try:
         while True:
-            cwd = str(os.path.basename(str(os.getcwd())))
-            username = str(getpass.getuser())
-            part1 = colored("[" + username + "@" + hostname, "cyan", attrs = ['bold'])
-            part2 = colored(cwd, "white", attrs = ["bold"])
-            part3 = colored("]$ ", "cyan", attrs = ["bold"])
-            command = input(part1 + " " + part2 + part3)
+            part1 = "[" + str(getpass.getuser()) + "@" + hostname + " "
+            part2 = str(os.path.basename(str(os.getcwd())))
+            part3 = "]$ "
+            input_required = [
+                ('class:username', part1),
+                ('class:dir', part2),
+                ('class:end', part3)
+            ]
+            command = session.prompt(input_required, style=style, lexer=PygmentsLexer(BashLexer), auto_suggest=AutoSuggestFromHistory())
+            
+            
             if command == "exit":
                 print("exit")
-                readline.append_history_file(1, history)
                 break
             elif command == "ls":
                 ls()
+            elif len(command) == 0:
+                pass
             elif command == "clear":
                 clear()
             elif command == "help":
@@ -36,7 +53,6 @@ def main():
             else:
                 execute_commands(command)
             command = command + "\n"
-            readline.append_history_file(1, history)
             
     except KeyboardInterrupt:
         print("\nexit")
