54d95ef07ce1c8fe95afc017d5f24aa532c618f6
Prateek Kesavarapu <thegifat@gmail.com>
2021-05-25 11:51:34 +0530

    "ignore"

--- build/lib/functions/shell.py
+++ build/lib/functions/shell.py
@@ -1,4 +1,4 @@
-from prompt_toolkit.shortcuts.prompt import CompleteStyle
+import subprocess
 from functions.functions import execute_commands, ls, clear
 from pathlib import Path
 from pygments.lexers.shell import FishShellLexer
@@ -14,13 +14,19 @@ import glob
 from prompt_toolkit.styles import Style
 import getpass
 import socket
-
+from git import Repo 
 
 def shell():
+            
     style = Style.from_dict({
-    'username': '#0FFFFF bold',
-    'dir': '#FFFFFF bold',
-    'end': '#0FFFFF bold'
+        'username': '#0FFFFF bold',
+        'dir': '#FFFFFF bold',
+        'mid': '#0FFFFF bold',
+        'parantheses1': '#FFFFFF bold',
+        'git-branch': '#FF0000 bold',
+        'star' : '#FFFF00 bold',
+        'parantheses2': '#FFFFFF bold',
+        'end': "#0FFFFF bold"
     })
     
     HOME_DIR = str(Path.home())
@@ -45,14 +51,54 @@ def shell():
             ])
             Files = WordCompleter(glob.glob("*"))
             
+            x = subprocess.getoutput("git rev-parse --is-inside-work-tree")
             merged_completers = merge_completers([bash_commands, Files])
-            part1 = "[" + str(getpass.getuser()) + "@" + hostname + " "
-            part2 = str(os.path.basename(str(os.getcwd())))
-            part3 = "]$ "
+
+            part1 = ""
+            part2 = ""
+            part3 = ""
+            part4 = ""
+            part5 = ""
+            part6 = ""
+            part7 = ""
+            part8 = ""
+            
+            if str(x) == "true":
+                repo = Repo(os.getcwd())
+                current_branch = subprocess.getoutput("git branch --show-current")
+                if len(repo.untracked_files) != 0:
+                    part1 = "[" + str(getpass.getuser()) + "@" + hostname + " "
+                    part2 = str(os.path.basename(str(os.getcwd())))
+                    part3 = "]"
+                    part4 = "("
+                    part5 = current_branch + " "
+                    part6 = "*"
+                    part7 = ")"
+                    part8 = "$ "
+                else:
+                    part1 = "[" + str(getpass.getuser()) + "@" + hostname + " "
+                    part2 = str(os.path.basename(str(os.getcwd())))
+                    part3 = "]"
+                    part4 = "("
+                    part5 = current_branch + " "
+                    part6 = "✓"
+                    part7 = " " + ")"
+                    part8 = "$ "                   
+            else:
+                part1 = "[" + str(getpass.getuser()) + "@" + hostname + " "
+                part2 = str(os.path.basename(str(os.getcwd())))
+                part3 = "]$ "
+                    
+                    
             input_required = [
                 ('class:username', part1),
                 ('class:dir', part2),
-                ('class:end', part3)
+                ('class:mid', part3),
+                ('class:parantheses1', part4),
+                ('class:git-branch', part5),
+                ('class:star', part6),
+                ('class:parantheses', part7),
+                ('class:end', part8)
             ]
             
             command = session.prompt(input_required,
@@ -60,7 +106,7 @@ def shell():
                                      lexer=PygmentsLexer(FishShellLexer),
                                      auto_suggest=AutoSuggestFromHistory(),
                                      completer=FuzzyCompleter(merged_completers),
-                                     complete_style=CompleteStyle.READLINE_LIKE)
+                                     complete_while_typing= False)
 
             if command == "exit":
                 print("exit")
@@ -82,7 +128,10 @@ def shell():
                         if out != None:
                             print(out)
                 except:
-                    execute_commands(command)
+                    try:
+                        execute_commands(command)
+                    except:
+                        os.system(command)
            
             command = command + "\n"
             
